fb389397f5ead719e2fcf06f36d7b3f33a6a928a
OfficialEseosa <raphaelomorose@gmail.com>
2024-11-19 23:09:16 -0500

    Increased the number of simulations and added more trackers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Import the random variable which we will use for this simulation
 import random
-import math
+import statistics
 
 # Create a Computer class which we will use to create Computer objects with an infected attribute
 # This attribute will let us know whether a Computer is infected or not
@@ -9,12 +9,15 @@ class Computer:
         self.infected = False
 
 days = []
-nsim = 1000
+no_of_computers_infected = []
+each_infected_once = []
+nsim = 100000
 
 for i in range(nsim):
     # Create two lists to store the non-infected and infected variables
     not_infected = []
     infected = []
+    contaminated = set()
 
     # Create 20 Computer objects
     for i in range(20):
@@ -25,6 +28,7 @@ for i in range(nsim):
     virus_computer.infected = True
     infected.append(virus_computer)
     not_infected.remove(virus_computer)
+    contaminated.add(virus_computer)
 
     # This happens on day 0
     day = 0
@@ -38,10 +42,12 @@ for i in range(nsim):
         # For each infected computer, infect each clean computer with a probability of 0.1
         for infected_comp in infected:
             for clean_comp in not_infected:
-                prob = random.random()
-                if prob <= 0.1:
-                    clean_comp.infected = True
-                    curr_infected.add(clean_comp)
+                if not clean_comp.infected:
+                    prob = random.random()
+                    if prob <= 0.1:
+                        clean_comp.infected = True
+                        curr_infected.add(clean_comp)
+                        contaminated.add(clean_comp)
         for comp in curr_infected:
             infected.append(comp)
             not_infected.remove(comp)
@@ -55,6 +61,14 @@ for i in range(nsim):
         infected = [computer for computer in infected if computer not in tech_choice]
         not_infected.extend(tech_choice)
     days.append(day)
+    no_of_computers_infected.append(len(contaminated))
+    if len(contaminated) == 20:
+        each_infected_once.append(1)
+    else:
+        each_infected_once.append(0)
+
 
 print(f"Solution after {nsim} simulations: ")
-print(f'Expected number of days to clean the system: {sum(days) / len(days)}')
+print(f'Expected number of days to clean the system: {statistics.mean(days)}')
+print(f'Probability that each computer gets infected at least once: {statistics.mean(each_infected_once)}')
+print(f'Expected number of computers that get infected: {statistics.mean(no_of_computers_infected)}')
